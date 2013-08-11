8dd2246b15d8afd9985608c52d72f18fff11e719
Austin <austin.keeley@gmail.com>
2013-08-11 07:52:25 -0400

    Testing out changes in cloud 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,6 @@ class User < ActiveRecord::Base
  	# is authenticated.
  	def self.authenticate(email, password)
  		user = find_by_email(email);
-
  		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt);
  			return user
  		else
