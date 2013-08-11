c05756ac08789e9af2dfc602aa26b5a3ac951609
OpenShift guest <brent@socialradar.com>
2013-08-11 12:12:19 -0400

    This is a git message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Vozlocale::Application.routes.draw do
     
     root :to => "display#index"
     get 'dashboard' => 'display#dashboard', :as => 'dashboard'
+    get 'supported_numbers' => 'display#supported_numbers', :as => 'supported_numbers'
     get 'log_in' => 'sessions#new', :as => 'log_in'
     get 'log_out' => 'sessions#destroy', :as => 'log_out'
     get 'sign_up' => 'users#new', :as => 'sign_up'
