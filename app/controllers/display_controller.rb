71955b6d3caf0da818b146293aa606b4318d6549
austinkeeley <austin.keeley@gmail.com>
2013-08-11 21:56:11 -0400

    Changes from sunday

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -9,7 +9,7 @@ class DisplayController < ApplicationController
   def dashboard
       
       # Get the last 10 messages from the db
-      @messages = Message.find(:all, :order => 'id desc', :limit => 20).reverse
+      @messages = Message.find(:all, :order => 'id desc', :limit => 20)
 
   end
 
